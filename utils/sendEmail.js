e7a160eaac7ca468f5d9b37457b9e9af19a42a34
AMSDEV345 <amsdev05@gmail.com>
2026-04-08 23:27:08 +0100

    restore auth routes and fix email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,24 +1,14 @@
 const { Resend } = require('resend');
-require('dotenv').config();
-
-if (!process.env.RESEND_API_KEY) {
-  throw new Error('RESEND_API_KEY not found! Check your .env file.');
-}
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
-  try {
-    await resend.emails.send({
-      from: `TaskFlow <${process.env.FROM_EMAIL}>`,
-      to,
-      subject,
-      html,
-    });
-    console.log('Email sent successfully to', to);
-  } catch (err) {
-    console.error('Error sending email:', err);
-  }
+  await resend.emails.send({
+    from: 'TaskFlow <onboarding@resend.dev>',
+    to,
+    subject,
+    html,
+  });
 };
 
 module.exports = sendEmail;
