795f70b3fa0c8a6cb4fbf87483bea86962acde99
AMSDEV345 <amsdev05@gmail.com>
2026-04-10 01:29:10 +0100

    switch to Resend for emails

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,14 @@
-  const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  await transporter.sendMail({
-    from: `"TaskFlow" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: process.env.FROM_EMAIL,
     to,
     subject,
     html,
   });
 };
 
-module.exports = sendEmail;
+module.exports = sendEmail; git 
